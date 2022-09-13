19addf30ebb8aa290e1b3ac25cca3bd6006bb0d5
Malets_Sergey <maletssergey@gmail.com>
2022-09-13 18:12:23 +0300

    last commit

--- app.py
+++ app.py
@@ -84,8 +84,41 @@ def create_article():
 
 @app.route('/posts')
 def posts():
-    articles = Article.query.order_by(Article.date).all()
+    articles = Article.query.order_by(Article.date.desc()).all()
     return render_template('posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article=article)
+
+@app.route('/posts/<int:id>/delete')
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "При удалении статьи произошла ошибка"
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == "POST":
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return 'При добавлении статьи произошла ошибка'
+    else:
+        article = Article.query.get(id)
+        return render_template('post_update.html', article=article)
 
 
 @app.route('/my_template')
